f275a3bcedb3b6c3a10a9be03bbecf6788d3fcee
oly1203 <razakamantsoahasina@gmail.com>
2022-08-23 15:21:39 +0300

    Ajout, liste,login
    ajout formulaire + liste des projet + login fa mbola amboariko tsara ny css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,8 @@ import '../styles/App.css';
 import Login from '../pages/Login';
 import Home from '../pages/Home';
 import Carte from '../components/Carte';
+import AjoutFormulaire from '../components/AjoutFormulaire';
+import ListeProjet from '../components/ListeProjet';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Adduction3d from './Adduction3d';
 import Axios, * as others from 'axios';
@@ -26,6 +28,8 @@ function App() {
 						<Route path="" element={<Carte />} />
 						<Route path="carte" element={<Carte />} />
 						<Route path="3d" element={<Adduction3d />} />
+						<Route path="liste" element={<ListeProjet />} />
+						<Route path="ajout" element={<AjoutFormulaire />} />
 					</Route>
 				</Routes>
 			</BrowserRouter>
