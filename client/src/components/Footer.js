0a8536410126a6636b4c4ddbbb167f4c08256423
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-08-30 16:13:12 +0300

    update map sidebar

--- client/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import '../styles/Footer.css'
-
-function Footer(){
-  return (  <div className='footer'> <h1>Footer</h1></div>)
-}
- 
-export default Footer
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+import App from "./components/app/App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
 );
-
-reportWebVitals();
