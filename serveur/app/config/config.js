05b1cfa0d85167cf7799fbfd1da935be084a2696
oly1203 <razakamantsoahasina@gmail.com>
2022-10-03 10:11:24 +0300

    update serveur

--- serveur/app/config/config.js
+++ serveur/app/config/config.js
@@ -2,22 +2,22 @@ require('dotenv').config(); // this is important!
 module.exports = {
     "development": { //test
         "username": "postgres",
-        "password": "mdpprom13",
-        "database": "adductioneau",
+        "password": "root",
+        "database": "projetMinistere",
         "host": "localhost",
         "dialect": "postgres"
     },
     "test": {
         "username": "postgres",
-        "password": "mdpprom13",
-        "database": "adductioneau",
+        "password": "root",
+        "database": "projetMinistere",
         "host": "127.0.0.1",
         "dialect": "postgres"
     },
     "production": {
         "username": "postgres",
-        "password": "mdpprom13",
-        "database": "adductioneau",
+        "password": "root",
+        "database": "projetMinistere",
         "host": "127.0.0.1",
         "dialect": "postgres"
     }
