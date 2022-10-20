53c3c94862de601f3c096d4762ae838050cee6bc
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-10-20 11:33:56 +0300

    update main

--- serveur/server.js
+++ serveur/server.js
@@ -4,15 +4,15 @@ const cors = require("cors");
 const app = express();
 const db = require("./app/models");
 
-// db.sequelize.sync({force:true})
-//   .then(() => {
-//     console.log("Synced db.");
-//   })
-//   .catch((err) => {
-//     console.log("Failed to sync db: " + err.message);
-//   });
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
 var corsOptions = {
-  origin: "http://localhost:8080",
+  origin: "http://localhost:8081",
   credentials: true
 };
 app.use(cors());
@@ -20,10 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 require("./app/routes/Projet.routes")(app);
-require("./app/routes/Materiel.routes")(app);
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
