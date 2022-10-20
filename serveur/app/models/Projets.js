4c82941221bd438f99c25d5c65795ec6c77b811c
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-10-20 14:24:30 +0300

    true update server

--- serveur/app/models/Projets.js
+++ serveur/app/models/Projets.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Projets.init({
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    id: { type: DataTypes.BIGINT(20), primaryKey: true, autoIncrement: true },
     id_utilisateur: DataTypes.INTEGER,
     region: DataTypes.STRING,
     district: DataTypes.STRING,
@@ -19,11 +19,12 @@ module.exports = (sequelize, DataTypes) => {
     latitude: DataTypes.FLOAT,
     longitude: DataTypes.FLOAT,
     nb_beneficiaire: DataTypes.FLOAT,
-    etat_ouvrage: DataTypes.BOOLEAN,
-    id_point_eau: DataTypes.INTEGER,
-    id_infra_eau: DataTypes.INTEGER,
-    id_role: DataTypes.INTEGER
+    point_eau: DataTypes.STRING,
+    infra_eau: DataTypes.STRING,
+    etat_ouvrage: DataTypes.STRING,
+    utilisation: DataTypes.STRING
   }, {
+    initialAutoIncrement: 1000,
     createdAt: false,
     updatedAt: false,
     sequelize,
