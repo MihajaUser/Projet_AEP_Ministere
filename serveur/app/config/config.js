5ce20ab25a36e9881c28925432a15fdd63c8676f
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-10-21 17:59:32 +0300

    update server

--- serveur/app/config/config.js
+++ serveur/app/config/config.js
@@ -2,21 +2,21 @@ require('dotenv').config(); // this is important!
 module.exports = {
     "development": { //test
         "username": "postgres",
-        "password": "mdpprom13",
+        "password": "root",
         "database": "projetMinistere",
         "host": "localhost",
         "dialect": "postgres"
     },
     "test": {
         "username": "postgres",
-        "password": "mdpprom13",
+        "password": "root",
         "database": "projetMinistere",
         "host": "127.0.0.1",
         "dialect": "postgres"
     },
     "production": {
         "username": "postgres",
-        "password": "mdpprom13",
+        "password": "root",
         "database": "projetMinistere",
         "host": "127.0.0.1",
         "dialect": "postgres"
--- serveur/app/config/db.config.js
+++ serveur/app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "postgres",
-    PASSWORD: "mdpprom13",
+    PASSWORD: "root",
     DB: "projetMinistere",
     dialect: "postgres",
     pool: {
