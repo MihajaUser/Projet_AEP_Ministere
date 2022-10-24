36b4b39ab60cce5bc70b5944be9523a3963e24ea
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-10-24 10:05:56 +0300

    update canalisation

--- client/src/guest/components/map/Map.js
+++ client/src/guest/components/map/Map.js
@@ -30,7 +30,7 @@ const Map = (props) => {
   >
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" m
     />
     <ScaleControl />
     <FeatureGroup>
