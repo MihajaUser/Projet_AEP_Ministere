ad6b6d4d7720e6ff918c68f9c0a41a605794099a
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-10-24 18:04:56 +0300

    canalisation 1.0

--- client/src/guest/components/mapCanal/Map.js
+++ client/src/guest/components/mapCanal/Map.js
@@ -1,17 +1,22 @@
 import React from "react";
 import L from "leaflet";
 import "./Style.css";
-
 require("leaflet-routing-machine");
 
 const style = {
   width: "100%",
-  height: "100%"
+  height: "95%"
 };
+
+const tabRoute = [];
+const canalisation = [
+  { "nom": "Tana1", "debutLat": -18.874121734092128, "debutLong": 47.506414002608906, "finLat": -18.873522767197834, "finLong": 47.51183206482083 },
+  { "nom": "Tana2", "debutLat": -18.876243480952564, "debutLong": 47.5049548809043, "finLat": -18.877329723732718, "finLong": 47.50936443252628 },
+  { "nom": "Tana3", "debutLat": -18.878568234958397, "debutLong": 47.50514799995343, "finLat": -18.878802454164994, "finLong": 47.50730907973457 }
+]
 
 class Map extends React.Component {
   componentDidMount() {
-    // create map
     this.map = L.map("map", {
       center: [-18.865447, 47.519533],
       zoom: 5.5,
@@ -22,52 +27,21 @@ class Map extends React.Component {
         })
       ]
     });
+    //initianlisation des canalisation 
+    canalisation.map((el) => (
+      tabRoute.push({
+        "route": L.Routing.control({ show: true, fitSelectedRoutes: true, plan: false, lineOptions: { styles: [{ color: "blue", opacity: "0.7", weight: 6 }] } }),
+        "debutLat": el.debutLat, "debutLong": el.debutLong, "finLat": el.finLat, "finLong": el.finLong
+      })
+    ))
 
-    var routeControl = L.Routing.control({
-      show: true,
-      fitSelectedRoutes: true,
-      plan: false,
-      lineOptions: {
-        styles: [
-          {
-            color: "blue",
-            opacity: "0.7",
-            weight: 6
-          }
-        ]
-      }
-    })
-      .addTo(this.map)
-      .getPlan();
-
-    var routeControl2 = L.Routing.control({
-      show: true,
-      fitSelectedRoutes: true,
-      plan: false,
-      lineOptions: {
-        styles: [
-          {
-            color: "green",
-            opacity: "0.7",
-            weight: 6
-          }
-        ]
-      }
-    })
-      .addTo(this.map)
-      .getPlan();
-
-    var newLatLngA = new L.LatLng(-18.880652545742503, 47.50655576416184, "taskA");
-    var newLatLngA1 = new L.LatLng(-18.879076187699845, 47.509762346609016, "taskB");
-
-    //-18.885426795728815, 47.50894252066036
-    //-18.886554961596023, 47.51020979012965
-
-    var newLatLngB = new L.LatLng(-18.885426795728815, 47.50894252066036, "taskA");
-    var newLatLngB1 = new L.LatLng(-18.886554961596023, 47.51020979012965, "taskB");
-
-    routeControl.setWaypoints([newLatLngA, newLatLngA1]);
-    routeControl2.setWaypoints([newLatLngB, newLatLngB1]);
+    //dessin des canalisation
+    tabRoute.map((el) => (
+      console.log("debut"),
+      el.route.addTo(this.map).getPlan(),
+      el.route.setWaypoints([new L.LatLng(el.debutLat, el.debutLong, "taskA"), new L.LatLng(el.finLat, el.finLong, "taskB")])
+    )
+    )
   }
   render() {
     return <div id="map" style={style} />;
