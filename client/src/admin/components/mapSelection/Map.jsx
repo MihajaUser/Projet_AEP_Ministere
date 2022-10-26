d950db59e83640a89adb03901d64214ea6a305a5
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-10-26 17:47:40 +0300

    zao zao zao

--- client/src/admin/components/mapSelection/Map.jsx
+++ client/src/admin/components/mapSelection/Map.jsx
@@ -3,6 +3,12 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import icon from "./constants";
 import { useState } from "react";
+import { useHistory } from 'react-router-dom';
+import Alert from 'react-bootstrap/Alert';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import * as MuiIcons from '@mui/icons-material';
 class Map extends Component {
   constructor(props) {
     super(props);
@@ -13,40 +19,50 @@ class Map extends Component {
     const { map } = this.state;
     if (prevState.map !== map && map) {
       map.on("click", function (e) {
-
-        alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng + " ");
-      });
+        this.setState({ debutLat: e.latlng.lat, debutLong: e.latlng.lng });
+        // alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng + " ");
+      }.bind(this));
     }
   }
   render() {
-    const DEFAULT_LATITUDE = -18.865447;
-    const DEFAULT_LONGITUDE = 47.519533;
-    const latitude = this.props.coords
-      ? this.props.coords.latitude
-      : DEFAULT_LATITUDE;
-    const longitude = this.props.coords
-      ? this.props.coords.latitude
-      : DEFAULT_LONGITUDE;
-
     return (
-      <MapContainer
-        className="leaflet-map"
-        center={[latitude, longitude]}
-        zoom={5.5}
-        scrollWheelZoom={true}
-        style={{ height: "100vh" }}
-        whenCreated={(map) => this.setState({ map })}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[latitude, longitude]} icon={icon}>
-          <Popup>Here you are ^_^{
-            // console.log("ito " + this.state.comment)
-          }</Popup>
-        </Marker>
-      </MapContainer>
+      <div>
+        <div className="MyMap">
+          <MapContainer
+            className="leaflet-map"
+            center={{ lat: -18.865447, lng: 47.519533 }}
+            zoom={5.5}
+            scrollWheelZoom={true}
+            style={{ height: "100vh", width: "50vh" }}
+            whenCreated={(map) => this.setState({ map })}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={{ lat: -18.865447, lng: 47.519533 }} icon={icon}>
+              <Popup>Here you are ^_^{
+                console.log("ito " + this.state.debutLat)
+              }</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+        <div className='CardContainer'>
+          <Card className='MyCard'>
+            <br></br>
+            <Link to="/adduction3d">
+              <Button className='MyButton' ><MuiIcons.ViewInAr />    3D Vue</Button>
+            </Link>
+            <br></br>
+            <Button className='MyButton'  ><MuiIcons.SelectAllTwoTone />   2D Vue</Button>
+            <br></br>
+            <Link to="/">
+              <Button className='MyButton'  ><MuiIcons.FmdGood />   CARTE</Button>
+            </Link>
+          </Card>
+        </div>
+      </div>
+
     );
   }
 }
