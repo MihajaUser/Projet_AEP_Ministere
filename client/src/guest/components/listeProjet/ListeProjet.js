e2f9c40f0220999a3c9209e5b0889b69c071ba84
OlyviaHasina <olyviahasina.razakamanantsoa@gmail.com>
2022-10-26 14:33:04 +0200

    update

--- client/src/guest/components/listeProjet/ListeProjet.js
+++ client/src/guest/components/listeProjet/ListeProjet.js
@@ -1,32 +1,53 @@
 import Table from 'react-bootstrap/Table';
 import React from 'react';
 import {Link} from 'react-router-dom'
+import { CrudService } from './../../../admin/components/CrudProjet/Crud.service';
+import { useState, useEffect} from 'react';
 // import {Routes,Route,Switch} from 'react-router-dom';
 function ListeProjet(){
+  const [listeProjet , setListeProjet] = useState([]);
+  const listeProjets = async () => {
+    const response = await CrudService. getAllProjet();
+    if(response.status === 200) {
+      setListeProjet(response.data);
+    }
+    else {
+      throw new Error("Failed to fetch users");
+    }
+  };
+   useEffect(() => {listeProjets()},[]);
     return (
       <div className="liste">
       <Table striped bordered hover>
       <thead>
         <tr>
-          <th>N°</th>
-          <th>Projet</th>
-          <th>Lieu</th>
-          <th>Etat</th>
+          <th>Numéro de projet</th>
+          <th>Nom de projet</th>
+          <th>Region</th>
+          <th>District</th>
+          <th>Commune</th>
+          <th>Fokontany</th>
+          <th>Localité</th>
+          <th>Etat d'ouvrage</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td><Link to='#'>Projet de Canalisation</Link></td>
-          <td>Itaosy</td>
-          <td>En cours</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td><Link to='#'>Installation de Réservoir d'eau</Link></td>
-          <td>Andoharanofotsy</td>
-          <td>En cours</td>
-        </tr>
+      {
+            listeProjet.map((item) =>{
+              return(
+                <tr>
+                <td>{item.id}</td>
+                <td>{item.utilisation}</td>
+                <td>{item.region}</td>
+                <td>{item.district}</td>
+                <td>{item.commune}</td>
+                <td>{item.fokontany}</td>
+                <td>{item.localite}</td> 
+                <td>{item.etat_ouvrage}</td>
+                </tr>
+              )
+            })
+      }
       </tbody>
     </Table>
     </div>
