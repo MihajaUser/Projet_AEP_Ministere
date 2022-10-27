608e4de97f5502bc88974e9c2e22c3fc309e49ff
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-10-27 12:13:53 +0300

    alefa barea

--- client/src/App.js
+++ client/src/App.js
@@ -12,13 +12,8 @@ import ListeProjet from './guest/components/listeProjet/ListeProjet'
 import Login from './guest/pages/login/Login';
 import Inscription from './guest/pages/login/Inscription';
 import Admin from './admin/pages/Main.jsx';
-
+import Citerne2d from './guest/components/citerne2d/Citerne2d';
 import MapCanal from './guest/components/mapCanal/Map';
-import MapSelection from './admin/components/mapSelection/Map';
-import MapEtape1 from './guest/components/mapSelection/MapEtape1';
-import MapEtape2 from './guest/components/mapSelection/MapEtape2';
-import MapEtape3 from './guest/components/mapSelection/MapEtape3';
-
 const App = () => {
   return (
     <Provider store={store}>
@@ -30,16 +25,12 @@ const App = () => {
             <Route path="adduction3d" element={<CiternePage />} />
             <Route path="listeProjet" element={<ListeProjet />} />
             <Route path="login" element={<Login />} />
-            <Route path="ajoutCanalisation1" element={<MapEtape1 />} />
-            <Route path="ajoutCanalisation2/:debutLat/:debutLong" element={<MapEtape2 />} />
-            <Route path="ajoutCanalisation3" element={<MapEtape3 />} />
             <Route path="canalisation" element={<MapCanal />} />
             <Route path="inscription" element={<Inscription />} />
-
+            <Route path="citerne2d/:latitude/:longitude/:region/:point_eau/:infra_eau" element={<Citerne2d />} />
           </Route>
           <Route path="admin/" element={<Admin />}>
             <Route path="" element={<  MapRouting />} />
-            <Route path="mapSelection" element={<MapSelection />} />
             <Route path="formulaire" element={<AjoutFormulaire />} />
             {/* <Route path="login" element={<Login />} />*/}
             <Route path="liste" element={<CrudProjet />} />
