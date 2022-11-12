122d37666d764eb0cec2c721b15d732c33997d6b
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-11-13 00:58:14 +0300

    recherche reservoir proche

--- client/src/guest/components/mapSelection/MapEtape1.jsx
+++ client/src/guest/components/mapSelection/MapEtape1.jsx
@@ -5,7 +5,6 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import * as MuiIcons from '@mui/icons-material';
-import * as GeometryUtil from "leaflet-geometryutil";
 import './styles.css'
 
 const MapEtape1 = (props) => {
@@ -52,42 +51,6 @@ const MapEtape1 = (props) => {
       });
     }
   }, [mapInstance]);
-
-  useEffect(() => {
-    nearestReservoir()
-  }, [])
-  const nearestReservoir = () => {
-    console.log("nearestReservoir")
-    const tab = [
-      { "lat": -18.97633509237672, "lng": 47.532593368495704, "nom": "fidy Andoharanofotsy" },
-      { "lat": -18.979973950086347, "lng": 47.533121489658186, "nom": "pharmacie andoharanofotsy" },
-      { "lat": -18.93891790675198, "lng": 47.521987974044215, "nom": " gastro sonerana" },
-      { "lat": - 12.303105648724799, "lng": 49.29392259399195, "nom": " Antsiranana" },
-      { "lat": - 18.914889469284635, "lng": 47.52474209363391, "nom": " Saint michel " },
-      { "lat": -18.915728372526345, "lng": 47.531181417949941, "nom": " Andohalo " }
-    ]
-    var iFrom;
-    var jFrom;
-    var iDistance = 0
-    var jDistance = 0
-    var minimum = tab[0];
-    for (let i = 0; i < tab.length; i++) {
-      for (let j = 0; j < tab.length; j++) {
-        iFrom = L.latLng([tab[i].lat, tab[i].lng])
-        jFrom = L.latLng([tab[j].lat, tab[j].lng])
-        iDistance = iFrom.distanceTo(([-18.91555964362533, 47.53035641290226]))
-        jDistance = jFrom.distanceTo(([-18.91555964362533, 47.53035641290226]))
-        if (iDistance > jDistance) {
-          minimum = tab[j]
-        }
-      }
-      console.log("le minimum")
-      console.log(minimum)
-      return minimum;
-    }
-
-  }
-
   const handleClick = () => {
     if (marker) {
       marker.removeFrom(mapInstance);
--- client/src/guest/components/mapSelection/MapEtape2.jsx
+++ client/src/guest/components/mapSelection/MapEtape2.jsx
@@ -3,11 +3,13 @@ import L from 'leaflet';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import { useSelector } from "react-redux";
 import Button from 'react-bootstrap/Button';
 import * as MuiIcons from '@mui/icons-material';
 import './styles.css'
 
 const MapEtape1 = (props) => {
+  const myStation = useSelector((state) => state.station)
   const { urlDebutLat, urlDebutLng } = useParams();
   const [finLat, setFinLat] = useState(null);
   const [finLng, setFinLng] = useState(null);
@@ -42,6 +44,7 @@ const MapEtape1 = (props) => {
       setFinLat(e.latlng.lat);
       setFinLng(e.latlng.lng);
     });
+    L.marker([urlDebutLat, urlDebutLng]).addTo(mapRef.current).bindPopup('Premiere localisation').openPopup();
     setMapInstance(mapRef.current);
   }, []);
 
@@ -66,16 +69,69 @@ const MapEtape1 = (props) => {
       marker.removeFrom(mapInstance);
       markerRef.current = null;
     } else {
-      markerRef.current = L.marker([finLat, finLng]).addTo(mapInstance);
+      markerRef.current = L.marker([finLat, finLng]).addTo(mapInstance).bindPopup('Deuxieme localisation').openPopup();
+    }
+    setMarker(markerRef.current);
+  };
+  const suggestion = () => {
+    const proche = nearestReservoir({ "latitude": urlDebutLat, "longitude": urlDebutLng }, myStation.stations)
+    setFinLat(proche.latitude);
+    setFinLng(proche.longitude);
+    if (marker) {
+      marker.removeFrom(mapInstance);
+      markerRef.current = null;
+    } else {
+      markerRef.current = L.marker([proche.latitude, proche.longitude]).addTo(mapInstance).bindPopup('Suggestion localisation').openPopup();
     }
     setMarker(markerRef.current);
   };
 
+  useEffect(() => {
+    // const tab = [
+    //   { "latitude": -18.97633509237672, "longitude": 47.532593368495704, "nom": "fidy Andoharanofotsy" },
+    //   { "latitude": -18.979973950086347, "longitude": 47.533121489658186, "nom": "pharmacie andoharanofotsy" },
+    //   { "latitude": -18.93891790675198, "longitude": 47.521987974044215, "nom": " gastro sonerana" },
+    //   { "latitude": - 12.303105648724799, "longitude": 49.29392259399195, "nom": " Antsiranana" },
+    //   { "latitude": - 18.914889469284635, "longitude": 47.52474209363391, "nom": " Saint michel " },
+    //   { "latitude": -18.915728372526345, "longitude": 47.531181417949941, "nom": " Andohalo " }
+    // ]
+    // var point = { "latitude": -13.90106312051247, "longitude": 48.07552129203595, "nom": " Manjakamiadana" }
+    // const minimum = nearestReservoir(point, myStation.stations)
+    // console.log("le minimum")
+    // console.log(minimum)
+    console.log("fin====================================")
+    console.log(finLat)
+    console.log(finLng)
+  }, [myStation, finLat, finLng])
+
+  const nearestReservoir = (onePoint, tab) => {
+    var iFrom;
+    var iDistance = 0
+    var iMinimum;
+    var minDistance = 0
+    var minimum = tab[0];
+    console.log("tab ")
+    console.log(tab.length)
+    for (let i = 0; i < tab.length; i++) {
+      iMinimum = L.latLng([minimum.latitude, minimum.longitude])
+      iFrom = L.latLng([tab[i].latitude, tab[i].longitude])
+      minDistance = iMinimum.distanceTo(([onePoint.latitude, onePoint.longitude]))
+      iDistance = iFrom.distanceTo(([onePoint.latitude, onePoint.longitude]))
+      if (minDistance > iDistance) {
+        minimum = tab[i]
+      }
+    }
+    console.log(minimum)
+    return minimum;
+  }
+
   return (
     <>
+
       <div id="map" style={mapStyles} />
       <div className='CardContainer'>
         <Card className='MyCard'>
+          <Button onClick={suggestion}> Reservoir Proche</Button>
           ETAPE 2
           <br></br>
           Emmplacement 1  : ville Antananrivo Commune Ambohijatovo
