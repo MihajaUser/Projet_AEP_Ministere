7b802994f09c118a4f0460cdbdeeee0278cdce93
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-11-13 13:08:32 +0300

    style succes

--- client/src/guest/components/mapSelection/Routing.jsx
+++ client/src/guest/components/mapSelection/Routing.jsx
@@ -29,7 +29,7 @@ const Routing = ({ debutLat, debutLng, finLat, finLng }) => {
     }).addTo(map);
     var legend = L.control({ position: "bottomleft" });
 
-    legend.onAdd = function(map) {
+    legend.onAdd = function (map) {
       var div = L.DomUtil.create("div", "legend");
       div.innerHTML += "<h4>Tegnforklaring</h4>";
       div.innerHTML += '<i style="background: #477AC2"></i><span>Water</span><br>';
@@ -38,13 +38,11 @@ const Routing = ({ debutLat, debutLng, finLat, finLng }) => {
       div.innerHTML += '<i style="background: #E8E6E0"></i><span>Residential</span><br>';
       div.innerHTML += '<i style="background: #FFFFFF"></i><span>Ice</span><br>';
       div.innerHTML += '<i class="icon" style="background-image: url(https://d30y9cdsu7xlg0.cloudfront.net/png/194515-200.png);background-repeat: no-repeat;"></i><span>Grænse</span><br>';
-      
-      
 
       return div;
-};
+    };
 
-legend.addTo(map);
+    legend.addTo(map);
     return () => map.removeControl(routingControl);
   }, [map]);
 
