1529549a095818e8f87761b926852a5e2a4acd0f
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-11-14 11:47:21 +0300

    routing update success

--- client/src/guest/components/mapCanal/Map.js
+++ client/src/guest/components/mapCanal/Map.js
@@ -2,15 +2,9 @@ import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import Routing from "../mapSelection/Routing";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
 const Map = (props) => {
   const myCanalisation = useSelector((state) => state.canalisation)
   const position = [-18.865447, 47.519533];
-  useEffect(() => {
-    console.log("dans canal==========")
-    console.log(myCanalisation.canalisations)
-  }, [myCanalisation])
-
   return (
     <MapContainer center={position} zoom={13} style={{ height: "100vh" }}>
       <TileLayer
@@ -19,7 +13,7 @@ const Map = (props) => {
       />
       {myCanalisation.canalisations.map((el) =>
       (
-        <Routing debutLat={el.debutLatitude} debutLng={el.debutLongitude} finLat={el.finLattitude} finLng={el.finLongitude} />
+        <Routing debutLat={el.debutLatitude} debutLng={el.debutLongitude} finLat={el.finLatitude} finLng={el.finLongitude} />
       ))}
     </MapContainer>
   );
