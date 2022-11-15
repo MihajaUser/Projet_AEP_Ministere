6cb9aaf73038b37b4aa32e2cd325c5527de5ea86
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-11-15 23:02:28 +0300

    legend to map success

--- client/src/guest/components/mapLegend/Legend.js
+++ client/src/guest/components/mapLegend/Legend.js
@@ -10,8 +10,8 @@ export default function Legend({ map }) {
       legend.onAdd = () => {
         const div = L.DomUtil.create("div", "info legend");
         div.innerHTML =
-          '<div><img src="' + MaisonIcon + '" width="30px" height="30px" /><h4>Station  </h4>' +
-          '<img src="' + citerneIcon + '" width="20px" height="30px" /><h4>Citerne</h4></div>';
+          '<div><img src="' + MaisonIcon + '" width="35px" height="40px" /><h4>Station Infrastructure eau </h4>' +
+          '<img src="' + citerneIcon + '" width="25px" height="40px" /><h4>Citerne Infrastructure eau</h4></div>';
         return div;
       };
       legend.addTo(map);
