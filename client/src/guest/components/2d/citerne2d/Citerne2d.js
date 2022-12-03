950a02567c48240dcc21b4f37976b03bbdbcf911
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-12-03 12:13:08 +0300

    manatsara loatra

--- client/src/guest/components/2d/citerne2d/Citerne2d.js
+++ client/src/guest/components/2d/citerne2d/Citerne2d.js
@@ -46,11 +46,17 @@ function Citerne2d() {
     reservoir.onload = () => {
       context.drawImage(reservoir, monImage.x, monImage.y, monImage.largeur, monImage.hauteur);
     };
+    // equateur
+    const equateur = { x: 400, y: 660, w: 650, h: 14 };
+    drawFillRect(equateur, { backgroundColor: 'rgb(99, 128, 191)' });
+    // greenwich
+    const greenwich = { x: 1045, y: 50, w: 14, h: 624 };
+    drawFillRect(greenwich, { backgroundColor: 'rgb(99, 128, 191)' });
     //trifangle verticale
     ctx.beginPath();
-    ctx.moveTo(1045, 310);
-    ctx.lineTo(1010, 360);
-    ctx.lineTo(1080, 360);
+    ctx.moveTo(1052, 310);
+    ctx.lineTo(1017, 360);
+    ctx.lineTo(1087, 360);
     ctx.fill();
     //triangle horizontal
     ctx.moveTo(630, 135);
@@ -61,7 +67,7 @@ function Citerne2d() {
     context.fillStyle = "rgb(232, 73, 73)";
     context.fill();
     //bar verticale
-    const barAltitude = { x: 1040, y: 360, w: 10, h: 250 };
+    const barAltitude = { x: 1047, y: 360, w: 10, h: 300 };
     drawFillRect(barAltitude, { backgroundColor: 'rgb(232, 73, 73)' });
     //bar horizontal
     const barHorizontal = { x: 680, y: 130, w: 370, h: 10 };
@@ -72,13 +78,9 @@ function Citerne2d() {
     //pointille verticale
     const pointilleV = { x: 627, y: 135, w: 5, h: 120 };
     drawFillRect(pointilleV, { backgroundColor: 'rgb(11, 238, 113)' });
-    //pointille noir verticale
-    // const pointillevN = { x: 1040, y: 135, w: 10, h: 175 };
-    // drawFillRect(pointillevN, { backgroundColor: 'rgb(1, 1, 3)' });
-    //pointille noir horizontal
     //mer
-    const lamer = { x: 450, y: 610, w: 600, h: 50 };
-    drawFillRect(lamer, { backgroundColor: 'rgb(99, 128, 191)' });
+    const lamer = { x: 450, y: 610, w: 595, h: 50 };
+    drawFillRect(lamer, { backgroundColor: 'rgb(88, 229, 241)' });
     getAltitude(latitude, longitude).then(rep => {
       //text gauche
       setShow(false)
@@ -86,14 +88,14 @@ function Citerne2d() {
       setAltitude(rep.data.results[0].elevation);
       const unite = 50
       let pied = rep.data.results[0].elevation;
-      ctx.font = '30px serif';
+      ctx.font = '25px serif';
       context.fillStyle = "rgb(11, 15, 88)";
-      ctx.fillText(`Région : ${region}`, 100, 110);
-      ctx.fillText(`Point eau  : ${point_eau}`, 100, 150);
-      ctx.fillText(`Infrastructure : ${infra_eau}`, 100, 190);
-      ctx.fillText(`Altitude : ${pied} m `, 100, 230);
-      ctx.fillText(`latitude : ${latitude}`, 100, 270);
-      ctx.fillText(`longitude : ${longitude}`, 100, 310);
+      ctx.fillText(`latitude : ${longitude}`, 100, 110);
+      ctx.fillText(`longitude : ${latitude}`, 100, 150);
+      ctx.fillText(`Altitude : ${pied} m `, 100, 190);
+      ctx.fillText(`Région : ${region}`, 100, 230);
+      ctx.fillText(`Point eau  : ${point_eau}`, 100, 270);
+      ctx.fillText(`Infrastructure : ${infra_eau}`, 100, 310);
       //text dessin
       context.fillStyle = "rgb(232, 73, 73)";
       ctx.fillText(`${latitude}`, 730, 120);
