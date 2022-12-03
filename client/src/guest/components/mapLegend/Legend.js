240eac3146e6fa2998592cbbb884136bae5348d9
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-12-03 23:41:25 +0300

    manatsara success 2

--- client/src/guest/components/mapLegend/Legend.js
+++ client/src/guest/components/mapLegend/Legend.js
@@ -11,7 +11,7 @@ export default function Legend({ map }) {
         const div = L.DomUtil.create("div", "info legend");
         div.innerHTML =
           '<div><img src="' + MaisonIcon + '" width="35px" height="40px" /><h4>Station Infrastructure eau </h4>' +
-          '<img src="' + citerneIcon + '" width="25px" height="40px" /><h4>Citerne Infrastructure eau</h4></div>';
+          '<img src="' + citerneIcon + '" width="25px" height="40px" /><h4>Reservoir Infrastructure eau</h4></div>';
         return div;
       };
       legend.addTo(map);
