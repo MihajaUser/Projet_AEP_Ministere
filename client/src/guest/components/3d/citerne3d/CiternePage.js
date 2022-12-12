e391597cc9f9a660b197fd1abd24768cffb6ff10
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-12-12 22:17:15 +0300

    sa

--- client/src/guest/components/3d/citerne3d/CiternePage.js
+++ client/src/guest/components/3d/citerne3d/CiternePage.js
@@ -8,13 +8,13 @@ function CiternePage() {
   return (
     <div className="maPage">
       <div className="my3D">
-        <Canvas pixelRatio={window.devicePixelRatio} camera={{ position: [20, 5, 5] }}>
-          <gridHelper args={[10, 10, `white`, `gray`]} position={[0, -10, 0]} />
+        <Canvas pixelRatio={window.devicePixelRatio} camera={{ position: [10, 7, 0] }}> {/* zoom , y,x */}
+          <gridHelper args={[10, 10, `white`, `gray`]} position={[0, 0, 0]} />
           <OrbitControls />
           <ambientLight intensity={0.6} />
           <pointLight position={[10, 10, 10]} />
           <Suspense fallback={null}>
-            <mesh position={[2.75, -2, 0]}>
+            <mesh position={[2.75, 8, 0]}>
               <Citerne />
             </mesh>
           </Suspense>
