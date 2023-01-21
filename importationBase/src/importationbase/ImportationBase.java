1dfd1755477b47f915fac153952136af8cac49cb
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2023-01-21 18:03:56 +0300

    update kely

--- importationBase/src/importationbase/ImportationBase.java
+++ importationBase/src/importationbase/ImportationBase.java
@@ -5,8 +5,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -25,7 +23,6 @@ public class ImportationBase {
         String cvsSplitBy = ";";
         Connection conn = null;
         Statement stmt = null;
-
         try {
             // Connect to the database
             conn
